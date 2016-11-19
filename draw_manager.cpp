8a1f138ec726b9e8824bf9190ec6f486aba3a376
20k <the_berronator@hotmail.co.uk>
2016-11-19 09:34:02 +0000

    fun with layouts

--- draw_manager.cpp
+++ draw_manager.cpp
@@ -14,26 +14,21 @@ std::string stringify_natural(float val)
     return to_string_prec(val * 100, 3);
 }
 
-void draw_manager::draw_entity_ui(entity_manager& entity_manage)
+void render_character_ui(character* c, int column_id)
 {
-    if(entity_manage.chars.size() == 0)
-        return;
-
-    character* c = entity_manage.chars[3];
-
     //Button(const char* label, const ImVec2& size = ImVec2(0,0));
     float button_width = 50.f;
     float half_sep = 15;
 
     float full_width = button_width*2 + half_sep;
 
-    ImGui::Begin("Peon");
+    ImGui::BeginGroup();
 
     ImGui::Button(c->name.c_str(), ImVec2(full_width, 0));
     ImGui::Button(c->classname.c_str(), ImVec2(full_width, 0));
 
-    if(c->classname != c->race.c_str())
-        ImGui::Button(c->race.c_str(), ImVec2(full_width, 0));
+    //if(c->classname != c->race.c_str())
+    ImGui::Button(c->race.c_str(), ImVec2(full_width, 0));
 
     int clevel = c->cur_level;
     int potential_level = c->get_raw_level_from_xp();
@@ -59,9 +54,11 @@ void draw_manager::draw_entity_ui(entity_manager& entity_manage)
 
     ImGui::Button(hp_str.c_str(), ImVec2(full_width, 0));
 
+    #define NO_COLUMNS
+    #ifndef NO_COLUMNS
     ImGui::Columns(2);
 
-    ImGui::SetColumnOffset(1, button_width + half_sep);
+    ImGui::SetColumnOffset(1, (button_width + half_sep) + (button_width + half_sep + half_sep) * column_id * 2);
 
     //ImGui::Button("LVL", ImVec2(button_width, 0));
 
@@ -90,6 +87,28 @@ void draw_manager::draw_entity_ui(entity_manager& entity_manage)
     }
 
     ImGui::Columns(1);
+    #endif
+
+    #ifdef NO_COLUMNS
+    stattable buffs = c->invent.get_buffs();
+
+    for(auto& i : c->stats)
+    {
+        std::string val = to_string_prec(i.val, 3);
+
+        float v2 = buffs.get_stat_val(i.key);
+
+        if(v2 > 0)
+            val = val + " (+" + to_string_prec(v2, 3) + ")";
+
+        ImGui::Button(i.key.c_str(), ImVec2(button_width + half_sep/4.f, 0));
+
+        //ImGui::SameLine(0.f, half_sep);
+        ImGui::SameLine();
+
+        ImGui::Button(val.c_str(), ImVec2(button_width + half_sep/4.f, 0));
+    }
+    #endif
 
     //std::string blk_chance = to_string_prec(c->get_block_chance()*100, 3);
 
@@ -109,6 +128,55 @@ void draw_manager::draw_entity_ui(entity_manager& entity_manage)
 
     ImGui::Text("%s", xp_str.c_str());
 
-    ImGui::End();
+    ImGui::EndGroup();
 
+    //printf("%f\n", ImGui::CalcItemWidth());
+}
+
+void render_test(character* c)
+{
+    ImGui::BeginGroup();
+    ImGui::Text("Text 1");
+    ImGui::Text("Text 2");
+
+    //ImGui::Columns(2);
+    //ImGui::Columns(1);
+
+    ImGui::EndGroup();
+
+    /*ImGui::NextColumn();
+
+    ImGui::BeginGroup();
+    ImGui::Text("Text 3");
+    ImGui::Text("Text 4");
+    ImGui::EndGroup();*/
+}
+
+void draw_manager::draw_entity_ui(entity_manager& entity_manage)
+{
+    if(entity_manage.chars.size() == 0)
+        return;
+
+    //character* c = entity_manage.chars[3];
+    ImGui::Begin("Peons");
+
+    //ImGui::Columns(entity_manage.chars.size());
+
+    for(int i=0; i<entity_manage.chars.size(); i++)
+    {
+        character* c = entity_manage.chars[i];
+
+        render_character_ui(c, i);
+
+        //render_test(c);
+
+        //if(i == entity_manage.chars.size()-1)
+            //ImGui::NextColumn();
+
+        ImGui::SameLine();
+    }
+
+    //ImGui::Columns(1);
+
+    ImGui::End();
 }
